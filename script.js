70cc52c2b9d239dfe429057863d97255e096f20a
Mikael Oliveira <mikaeloliveiradev@gmail.com>
2024-12-09 14:51:54 -0300

    Add shadow to blocks

--- script.js
+++ script.js
@@ -214,7 +214,8 @@ class Block {
     let y = this.y;
     let width = this.width;
     let radius = 10;
-
+    
+    // Draw the rounded corner square
     ctx.beginPath();
     ctx.moveTo(x + radius, y);
     ctx.lineTo(x + width - radius, y);
@@ -227,6 +228,41 @@ class Block {
     ctx.quadraticCurveTo(x, y, x + radius, y);
     ctx.fillStyle = this.color;
     ctx.fill();
+    ctx.closePath();
+    
+    // Add the shadow inside the bottom edge
+
+    /* The coordinates of the shadow area are as the following:
+    c0─────c5    - c0 represents the coordinate x0 and y0.
+    │       │    - if a value of x is used in c0 and c1,
+    c1      c4     then x is named x01.
+    ╰─c2─c3─╯
+    */
+    let x01 = x
+    let x2 = x + radius
+    let x3 = x + width - radius
+    let x45 = x + width
+    let y05 = y + width*.8 // the top point of the shadow area
+    let y14 = y + width - radius
+    let y23 = y + width
+
+    let shadowGradient = ctx.createLinearGradient(x, y + width, x, y05);
+    shadowGradient.addColorStop(0, "rgba(0, 0, 0, 0.25)");
+    shadowGradient.addColorStop(1, "rgba(0, 0, 0, 0.01)");
+
+    ctx.fillStyle = shadowGradient;
+    
+    ctx.beginPath();
+    ctx.moveTo(x01, y05);
+    ctx.lineTo(x01, y14);
+    ctx.quadraticCurveTo(x, y + width, x2, y23);
+    ctx.lineTo(x3, y23);
+    ctx.quadraticCurveTo(x + width, y + width, x45, y14);
+    ctx.lineTo(x45, y05);
+    ctx.fill();
+    //ctx.strokeStyle = "red"
+    //ctx.lineWidth = 2
+    //ctx.stroke()
     ctx.closePath();
   }
 }
@@ -307,5 +343,4 @@ for (let row = grid.verLength - 1; row > grid.verLength - 10; row--) {
     composition.include(block, 4);
   }
 }
-
 update();
